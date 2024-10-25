c83087857483b462c1a5fa66386291adc08310a0
Ectomilas <salim.zidane44@hotmail.com>
2024-10-25 17:09:30 +0200

    Delete inverseChaineFonctionnel.py

--- inverseChaineFonctionnel.py
+++
@@ -1,8 +0,0 @@
-def inverseTexteFonctionnel(texte):
-    if texte == "":
-        return ""
-    else:
-        return inverseTexteFonctionnel(texte[1:]) + texte[0]
-      
-# Exemple
-print(inverseTexteFonctionnel("abcd"))  # Result : "dcba"
