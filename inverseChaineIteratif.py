7a3339127ec30b75748ab43cf36cb011ef58e8c8
Ectomilas <salim.zidane44@hotmail.com>
2024-10-25 17:12:44 +0200

    Delete inverseChaineIteratif.py

--- inverseChaineIteratif.py
+++
@@ -1,8 +0,0 @@
-def inverseTexteIteratif(texte):
-    inverse = ""
-    for char in texte:
-        inverse = char + inverse
-    return inverse
-
-# Exemple d'utilisation
-print(inverseTexteIteratif("abcd"))  # Résultat attendu : "dcba"
